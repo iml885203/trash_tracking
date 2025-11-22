2e393f5e5efa08e09959576ab130ee700dc6880a
iml885203 <iml885203@gmail.com>
2025-11-23 00:55:36 +0800

    refactor: update core package __init__.py (Task 2.5)
    - Export all public APIs from core package
    - Add comprehensive __all__ list
    - Organize exports by category (clients, models, core, utils)
    
    Part of monorepo migration Phase 2

--- packages/core/trash_tracking_core/__init__.py
+++ packages/core/trash_tracking_core/__init__.py
@@ -1,3 +1,51 @@
 """Trash Tracking Core Package - Shared logic for all applications."""
 
 __version__ = "0.1.0"
+
+# Import all public APIs
+from trash_tracking_core.clients import NTPCApiClient, NTPCApiError
+from trash_tracking_core.core import (
+    MatchResult,
+    PointMatcher,
+    StateManager,
+    TruckState,
+    TruckTracker,
+)
+from trash_tracking_core.models import Point, PointStatus, TruckLine
+from trash_tracking_core.utils import (
+    AddressGeocoder,
+    CollectionPointRecommendation,
+    ConfigError,
+    ConfigManager,
+    GeocodingError,
+    GeocodingProvider,
+    RouteAnalyzer,
+    logger,
+)
+
+__all__ = [
+    # Version
+    "__version__",
+    # Clients
+    "NTPCApiClient",
+    "NTPCApiError",
+    # Models
+    "Point",
+    "PointStatus",
+    "TruckLine",
+    # Core
+    "TruckTracker",
+    "StateManager",
+    "TruckState",
+    "PointMatcher",
+    "MatchResult",
+    # Utils
+    "ConfigManager",
+    "ConfigError",
+    "logger",
+    "AddressGeocoder",
+    "GeocodingError",
+    "GeocodingProvider",
+    "RouteAnalyzer",
+    "CollectionPointRecommendation",
+]
